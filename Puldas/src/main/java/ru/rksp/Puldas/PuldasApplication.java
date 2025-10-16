b003e24f1d38e93b6fbd707288286ba047c35f8f
Sashaminiral <sashapuldas@mail.ru>
2025-10-16 23:21:13 +0500

    1

---
+++ Puldas/src/main/java/ru/rksp/Puldas/PuldasApplication.java
@@ -0,0 +1,13 @@
+package ru.rksp.Puldas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuldasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuldasApplication.class, args);
+	}
+
+}
